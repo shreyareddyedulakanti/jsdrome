aca847b9e48d588f43e319582f0d256f5c1635dc
shreyareddyedulakanti <e.shreyareddy@gmail.com>
2020-01-26 00:36:33 +0530

    feat: firebase functions

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Profile from './profile';
-import profileData from '../../profile.json';
+import Counter from './counter';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>JS Drome</h1>
-        <Profile profile={profileData}/>
+        <Counter num={4}/>
       </div>
     );
   }
